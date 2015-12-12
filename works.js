d9c43444b6d6ae131385ad96180dbce21a67e8d5
ntcode <ntskupa@gmail.com>
2015-12-11 20:22:24 -0600

    Update works.js

--- works.js
+++ works.js
@@ -3,41 +3,41 @@ var main = function() {
     $('.dropdown-toggle').click(function() {
       $('.dropdown-menu').toggle(); 
 });
-    /*clicking forward arrow moves forward to next picture-slide & next-dot, then starts back at first slide & dot*/
+    /*clicking forward arrow moves forward to next picture-slide & next-box, then starts back at first slide & box*/
     $('.arrow-next').click(function() {
       var currentSlide = $('.active-slide');
       var nextSlide = currentSlide.next();
-      var currentDot = $('.active-dot');
-      var nextDot = currentDot.next();
+      var currentBox = $('.active-Box');
+      var nextBox = currentBox.next();
       
       if(nextSlide.length == 0) {
         nextSlide = $('.slide').first();
 }
-      if(nextDot.length == 0) {
-        nextDot = $('.dot').first();
+      if(nextBox.length == 0) {
+        nextBox = $('.box').first();
 }
       currentSlide.fadeOut(600).removeClass('active-slide');
       nextSlide.fadeIn(600).addClass('active-slide');
-      currentDot.removeClass('active-dot');
-      nextDot.addClass('active-dot');
+      currentBox.removeClass('active-box');
+      nextBox.addClass('active-box');
 });
-    /*clicking back arrow moves back to previous picture-slide & previous-dot*/
+    /*clicking back arrow moves back to previous picture-slide & previous-box*/
     $('.arrow-prev').click(function() {
       var currentSlide = $('.active-slide');
       var prevSlide = currentSlide.prev();
-      var currentDot = $('.active-dot');
-      var prevDot = currentDot.prev();
+      var currentBox = $('.active-box');
+      var prevBox = currentBox.prev();
       
       if(prevSlide.length == 0) {
         prevSlide = $('.slide').last();
 }
-      if(prevDot.length == 0) {
-        prevDot = $('.dot').last();
+      if(prevBox.length == 0) {
+        prevBox = $('.box').last();
 }
       currentSlide.fadeOut(600).removeClass('active-slide');
       prevSlide.fadeIn(600).addClass('active-slide');
-      currentDot.removeClass('active-dot');
-      prevDot.addClass('active-dot');
+      currentBox.removeClass('active-box');
+      prevBox.addClass('active-box');
 });
 }
 $(document).ready(main); 
